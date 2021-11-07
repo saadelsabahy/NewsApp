515d967aca41fe1b9df5b073277622e48d076513
Saad Elsabahy <saadelsabahy11@gmail.com>
2021-11-07 18:50:32 +0200

    fix:date and author style

--- src/components/NewsCard/index.tsx
+++ src/components/NewsCard/index.tsx
@@ -2,7 +2,7 @@ import {CommonStyles, Spaces} from '@constants/style';
 import {calcFont} from '@constants/style/sizes';
 import * as React from 'react';
 import {StyleSheet} from 'react-native';
-import {Card, Paragraph, useTheme} from 'react-native-paper';
+import {Card, Paragraph, Text, Title, useTheme} from 'react-native-paper';
 
 interface Props {
   onCardPressed: () => void;
@@ -19,16 +19,16 @@ const NewsCard = ({onCardPressed, title, author, date, imageSource}: Props) => {
     <Card style={styles.container} onPress={onCardPressed}>
       <Card.Cover
         source={{uri: imageSource}}
-        style={{backgroundColor: primary}}
+        //style={{backgroundColor: primary}}
       />
       <Card.Title
         titleNumberOfLines={2}
         titleStyle={[CommonStyles.title, styles.title]}
         title={title}
       />
-      <Card.Content>
-        <Paragraph>{author}</Paragraph>
-        <Paragraph style={{alignSelf: 'flex-end'}}>{date}</Paragraph>
+      <Card.Content style={styles.cardContent}>
+        <Text style={styles.paragraph}>{author}</Text>
+        <Text style={[styles.paragraph]}>{date}</Text>
       </Card.Content>
     </Card>
   );
@@ -38,8 +38,23 @@ const styles = StyleSheet.create({
     margin: Spaces.medium,
     borderRadius: calcFont(7),
     overflow: 'hidden',
+    marginBottom: 0,
   },
-  title: {marginTop: calcFont(15), letterSpacing: 0},
+  cardContent: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 0,
+
+    paddingBottom: 0,
+  },
+  title: {marginTop: Spaces.medium, letterSpacing: 0},
+  paragraph: {
+    textAlignVertical: 'center',
+    paddingVertical: Spaces.small,
+    opacity: 0.5,
+  },
 });
 
 export {NewsCard};
